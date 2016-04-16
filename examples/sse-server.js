6e3c93f0c10de004db491fa3585ad7d2accec47b
Nik Katalnikov <prometheusaudio@gmail.com>
2016-04-16 13:26:49 +0300

    npm scripts and sae-server

---
+++ examples/sse-server.js
@@ -0,0 +1,35 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  if (req.headers.accept && req.headers.accept == 'text/event-stream') {
+    if (req.url == '/events') {
+      sendSSE(req, res);
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  }
+}).listen(3002);
+
+console.log('Sample SSE server, port 3002');
+
+function sendSSE(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+
+  var id = (new Date()).toLocaleTimeString();
+
+  setInterval(function () {
+    constructSSE(res, id, (new Date()).toLocaleTimeString());
+  }, 5000);
+
+  constructSSE(res, id, (new Date()).toLocaleTimeString());
+}
+
+function constructSSE(res, id, data) {
+  res.write('id: ' + id + '\n');
+  res.write("data: " + data + '\n\n');
+}
