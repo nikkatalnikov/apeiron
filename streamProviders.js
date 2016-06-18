2e28f77895b651664845861664c1c399eb650386
Nik Katalnikov <prometheusaudio@gmail.com>
2016-06-18 16:50:39 +0300

    fix spread

--- streamProviders.js
+++ streamProviders.js
@@ -49,7 +49,7 @@ class HTTPProvider extends StreamProvider {
     Observable
       .from(list)
       .concatMap(x => Observable.of(x).delay(delay))
-      .subscribe([alias, data] => this.send(alias, data))
+      .subscribe(([alias, data]) => this.send(alias, data))
   }
 }
 
